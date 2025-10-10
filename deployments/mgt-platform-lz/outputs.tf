82adde5f2846836c6d642fbb5aba79c6705f0b0a
Tim Shand <tim@tshand.com>
2025-10-10 21:22:50 +1300

    fix: Modified outputs from PLZ networking to new name format.

--- deployments/mgt-platform-lz/outputs.tf
+++ deployments/mgt-platform-lz/outputs.tf
@@ -1,4 +1,9 @@
+output "plz_hub_vnet_rg" {
+  description = "The name of the hub VNet."
+  value = module.plz-con-network-hub.plz_hub_vnet_rg
+}
+
 output "plz_hub_vnet_name" {
   description = "The name of the hub VNet."
-  value = module.az-con-network-hub.plz_hub_vnet_name
+  value = module.plz-con-network-hub.plz_hub_vnet_name
 }
